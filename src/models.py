e4914f9a894730e3057c07a01b8507909ad438cf
CataFlores19 <cataflores85@gmail.com>
2025-09-30 16:45:06 -0300

    Modelos implementados

--- src/models.py
+++ src/models.py
@@ -4,16 +4,147 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(String(255), nullable=False)
+    type: Mapped[str] = mapped_column(
+        String(50), nullable=False)  # e.g., 'image', 'video'
+    post_id: Mapped[int] = mapped_column(
+        db.ForeignKey('post.id'), nullable=False)
+    post: Mapped["Post"] = db.relationship("Post", back_populates="media")
+    created_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp())
+    updated_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "url": self.url,
+            "type": self.type,
+            "post_id": self.post_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    caption: Mapped[str] = mapped_column(String(255), nullable=True)
+    media_id: Mapped[int] = mapped_column(
+        db.ForeignKey('media.id'), nullable=True)
+    media: Mapped["Media"] = db.relationship(
+        "Media", back_populates="post", uselist=False)
+    user_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    user: Mapped["User"] = db.relationship("User", back_populates="posts")
+    created_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp())
+    updated_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "media_id": self.media_id,
+            "caption": self.caption,
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String(255), nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(
+        db.ForeignKey('post.id'), nullable=False)
+    user: Mapped["User"] = db.relationship("User", back_populates="comments")
+    post: Mapped["Post"] = db.relationship("Post", back_populates="comments")
+    created_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp())
+    updated_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    user_to: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    created_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_from": self.user_from,
+            "user_to": self.user_to,
+            "created_at": self.created_at,
+        }
+
+
+class Like(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(
+        db.ForeignKey('post.id'), nullable=False)
+    created_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "created_at": self.created_at,
+        }
+
+
+class SavedPost(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(
+        db.ForeignKey('post.id'), nullable=False)
+    created_at: Mapped[str] = mapped_column(
+        db.DateTime, default=db.func.current_timestamp())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "created_at": self.created_at,
         }
